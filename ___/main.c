e7f71652d476eb0c53f806782080c777d525015a
Bastola <messiniki@outlook.com>
2022-05-02 08:39:32 +0545

    may 2 update

---
+++ ___/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter any three number: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b) {
+        if (a > c) {
+            printf("%d is the greatest number!", a);
+        } else {
+            printf("%d is the greatest number!", c);
+        }
+    } else if (b > c) {
+        printf("%d is the greatest number!", b);
+    } else {
+        printf("%d is the greatest number!", c);
+    }
+    return 0;
+}
