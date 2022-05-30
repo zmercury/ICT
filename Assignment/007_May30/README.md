06507db721d32a96b1d27eef76c03dba8f047670
Bastola <messiniki@outlook.com>
2022-05-30 08:42:11 +0545

    may 30 update

--- Assignment/007_May30/README.md
+++ Assignment/007_May30/README.md
@@ -64,6 +64,47 @@ int main() {
             break;
         }
         default: {
+            printf("Invalid Input!");
+            break;
+        }
+    }
+    return 0;
+}
+```
+
+
+``` c
+#include <stdio.h>
+
+int main() {
+    int choice,l,b,h;
+
+    printf("Enter 1 for area of room:\n ");
+    printf("Enter 2 for volume of room:\n  ");
+    printf("Enter 3 for perimeter:\n ");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice) {    
+        case 1: {
+            printf("Enter length and breath of the room: ");
+            scanf("%d %d", &l, &b);
+            printf("Area = %dcm2\n", l * b);
+            break;
+        }
+        case 2: {
+            printf("Enter length, breath and height of the room: ");
+            scanf("%d %d %d", &l, &b, &h);
+            printf("Volume = %dcm3\n", l * b * h);
+            break;
+        }
+        case 3: {
+            printf("Enter length and breath of the room: ");
+            scanf("%d %d", &l, &b);
+            printf("Perimeter = %dcm\n", 2 * (l + b));
+            break;
+        }
+        default: {
             break;
         }
     }
--- Assignment/007_May30/main.c
+++ Assignment/007_May30/main.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int choice;
+    int choice,l,b,h;
+
+    printf("Enter 1 for area of room:\n ");
+    printf("Enter 2 for volume of room:\n  ");
+    printf("Enter 3 for perimeter:\n ");
     printf("Enter your choice: ");
     scanf("%d", &choice);
 
-    switch(choice) {
+    switch(choice) {    
         case 1: {
-            printf("Sunday!");
+            printf("Enter length and breath of the room: ");
+            scanf("%d %d", &l, &b);
+            printf("Area = %dcm2\n", l * b);
             break;
-        } 
+        }
         case 2: {
-            printf("Monday!");
+            printf("Enter length, breath and height of the room: ");
+            scanf("%d %d %d", &l, &b, &h);
+            printf("Volume = %dcm3\n", l * b * h);
             break;
         }
         case 3: {
-            printf("Tuesday!");
-            break;
-        }
-        case 4: {
-            printf("Wednesday!");
-            break;
-        }
-        case 5: {
-            printf("Thursday!");
-            break;
-        }
-        case 6: {
-            printf("Friday!");
-            break;
-        }
-        case 7: {
-            printf("Saturday!");
+            printf("Enter length and breath of the room: ");
+            scanf("%d %d", &l, &b);
+            printf("Perimeter = %dcm\n", 2 * (l + b));
             break;
         }
         default: {
