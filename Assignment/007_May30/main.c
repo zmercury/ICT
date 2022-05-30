d320da28542e8059488d2edda039eeb62f2a352a
Bastola <messiniki@outlook.com>
2022-05-30 09:10:40 +0545

    may 30 update

--- Assignment/007_May30/main.c
+++ Assignment/007_May30/main.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
-
-int main() {
-    int choice,l,b,h;
-
-    printf("Enter 1 for area of room:\n ");
-    printf("Enter 2 for volume of room:\n  ");
-    printf("Enter 3 for perimeter:\n ");
-    printf("Enter your choice: ");
+#include<stdio.h>
+int main()
+{
+    int choice, l, b, h;
+    printf("Enter 1 for area \n");
+    printf("Enter 2 for volume of the room \n");
+    printf("Enter 3 for perimeter \n");
     scanf("%d", &choice);
-
-    switch(choice) {    
-        case 1: {
-            printf("Enter length and breath of the room: ");
-            scanf("%d %d", &l, &b);
-            printf("Area = %dcm2\n", l * b);
+    
+    switch(choice)
+    {
+        case 1:
+            printf("Enter length and breadth");
+            scanf("%d %d", &l,&b);
+            printf("Area= %dCm2 \n", l * b);
             break;
-        }
-        case 2: {
-            printf("Enter length, breath and height of the room: ");
-            scanf("%d %d %d", &l, &b, &h);
-            printf("Volume = %dcm3\n", l * b * h);
+        case 2:
+            printf("Enter length and breadth and height");
+            scanf("%d %d %d", &l,&b,&h);
+            printf("VOLUME= %dCm3 \n", l * b * h);
             break;
-        }
-        case 3: {
-            printf("Enter length and breath of the room: ");
-            scanf("%d %d", &l, &b);
-            printf("Perimeter = %dcm\n", 2 * (l + b));
+        case 3:    
+            printf("Enter length and breadth");
+            scanf("%d %d", &l , &b);
+            printf("Perimeter=%d Cm \n", 2* (l +b));
             break;
-        }
-        default: {
-            break;
-        }
+        default:
+            printf("Invalid info!");
+            break;      
     }
-    return 0;
 }
