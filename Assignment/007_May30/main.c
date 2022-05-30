8f1d1f20ff13ab12a733ea1cf5e19b54c0809d27
Bastola <messiniki@outlook.com>
2022-05-30 08:22:45 +0545

    may 30 update

--- Assignment/007_May30/main.c
+++ Assignment/007_May30/main.c
@@ -1,32 +1,3 @@
-#### May 30
-
-## Programming
-
-
-### Switch Case statement 
-Switch case statement test ant expression and execites the statement ot case equivalent to the expression. If none f the case meets the expression then default block gets executed.
-
-Syntax: 
-``` c
-switch (expression) {
-    case 1: {
-        statement 1;
-    }
-    case 2: {
-        statement 2;
-    }
-    case n: {
-        statement n;
-    }
-    default: {
-        statement;
-    }
-}
-```
-
-Program 
-
-``` c 
 #include <stdio.h>
 
 int main() {
@@ -69,4 +40,3 @@ int main() {
     }
     return 0;
 }
-```
