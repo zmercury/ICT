1d03909682f162182879abeb40321e381acec232
Bastola <messiniki@outlook.com>
2022-06-10 08:44:10 +0545

    10th june commit

--- Notes/016_June10/README.md
+++ Notes/016_June10/README.md
@@ -22,5 +22,33 @@ int main() {
     printf("Sum  = %d",sum);       
     return 0;  
 }      
+```
+
+2. Write a program to enter ant four digits even number and display sum of its all digits
+
+```c
+
+#include <stdio.h>
+
+int main() {
+
+    int n,sum=0,m;  
+
+    printf("Enter a number:");    
+    scanf("%d",&n);    
+
+    if (n > 999 && n < 10000) {
+        while(n>0) {    
+            m=n%10;    
+            sum=sum+m;    
+            n=n/10;    
+        }
+        printf("Sum = %d",sum);       
+    } else {
+        printf("Enter a four digit number!");
+    }
+
+    return 0;  
+}      
 
 ```
--- Notes/016_June10/main.c
+++ Notes/016_June10/main.c
@@ -1,20 +1,24 @@
-//Wap to enter any number and display sum of its digit
+//wap to enter ant four digits even number and display sum of its all digits
 
 #include <stdio.h>
 
 int main() {
+
     int n,sum=0,m;  
 
     printf("Enter a number:");    
     scanf("%d",&n);    
 
-    while(n>0) {    
-        m=n%10;    
-        sum=sum+m;    
-        n=n/10;    
+    if (n > 999 && n < 10000) {
+        while(n>0) {    
+            m=n%10;    
+            sum=sum+m;    
+            n=n/10;    
+        }
+        printf("Sum = %d",sum);       
+    } else {
+        printf("Enter a four digit number!");
     }
 
-    printf("Sum  = %d",sum);  
-    
     return 0;  
 }      
