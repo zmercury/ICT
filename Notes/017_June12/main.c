e22ba74e0adad75e59cc0fd66bcaa4d1ba8bdb0d
Bastola <messiniki@outlook.com>
2022-06-12 08:16:30 +0545

    12th june commit

--- Notes/017_June12/main.c
+++ Notes/017_June12/main.c
@@ -1,5 +1,21 @@
+//Wap to enter any number and display its reverse order
+
 #include <stdio.h>
 
 int main() {
+
+    int n,sum=0,m;  
+
+    printf("Enter a number:");    
+    scanf("%d",&n);    
+
+    while(n>0) {    
+        m=n%10;   
+        sum=sum*10+m;     
+        n=n/10;    
+    }
     
-}
+    printf("The reverse is %d",sum);       
+
+    return 0;  
+}    
