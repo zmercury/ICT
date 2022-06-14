7b4b4695269b6e2526b777f7f7bb23aa8761a25d
Bastola <messiniki@outlook.com>
2022-06-14 08:40:31 +0545

    14th june commit

---
+++ Notes/018_June14/main.c
@@ -0,0 +1,23 @@
+//Wap to input any number and check if it is prime or composite.
+
+#include <stdio.h>
+
+int main() {
+    int number,inc,i;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    for (i = 1; i <= number; i++) {
+        if(number%i==0) {
+            inc = inc + 1;
+        }
+    }
+    if(inc == 2) {
+        printf("%d is prime number!", number);
+    } else {
+        printf("%d is composite number!", number);
+    }
+
+    return 0;
+} 
